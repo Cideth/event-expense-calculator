9522a6a69d5ba038ddb1b0a82d155a87b656cef4
bm150 <jbm20dev@gmail.com>
2024-10-11 18:16:35 +0900

    chore : 스켈레톤 UI 작성 및 기타 주석 추가

---
+++ src/components/GatherRoomSidebar.tsx
@@ -0,0 +1,11 @@
+// // 해당 컴포넌트에서 할 일
+
+// 참석자의 상태(관리자 여부, 현재 참석 여부)를 포함한 참석자 목록과 초대 링크를 복사 할 수 있는 버튼 컴포넌트
+
+export function InviteesList() {
+  return <div>InviteesList</div>;
+}
+
+export function InviteButton() {
+  return <div>InviteButton</div>;
+}
