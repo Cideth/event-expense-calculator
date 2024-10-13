83c9049f4592dca0d02d49595304637b6658d535
bm150 <jbm20dev@gmail.com>
2024-10-14 06:16:31 +0900

    feat : 사이드바 닫기 버튼 추가 및 방 설정 이동 링크 구현

--- src/app/gatherings/[roomId]/SlidingSidebar.tsx
+++ src/app/gatherings/[roomId]/SlidingSidebar.tsx
@@ -6,6 +6,12 @@ import UserAvatar from "./UserAvatar";
 import { BsShare } from "react-icons/bs";
 import { RxExit } from "react-icons/rx";
 import ShareModal from "@/components/ShareModal";
+import { useParams } from "next/navigation";
+import { IoIosSettings } from "react-icons/io";
+import { BiSolidDrink } from "react-icons/bi";
+import { MdOutlineNoDrinks } from "react-icons/md";
+import Link from "next/link";
+
 export default function SlidingSidebar({
   isOpen,
   onClose,
@@ -14,13 +20,34 @@ export default function SlidingSidebar({
   onClose: () => void;
 }) {
   const [show, setShow] = useState(false);
+  const { roomId } = useParams();
   const setModalClose = () => {
     setShow(false);
   };
   return (
     <>
-      <div className={`sidebar ${isOpen ? "open" : ""}`}>
-        <h2>유저 목록</h2>
+      <div
+        className={`sidebar ${
+          isOpen ? "open" : ""
+        } col-9 col-sm-9 col-md-6 col-xl-4`}
+      >
+        <div className="d-flex justify-content-between">
+          <div className="col-8">
+            <h2 className="">유저 목록</h2>
+          </div>
+          <div className="col-4 text-end">
+            <Button
+              variant="outline-secondary"
+              size="lg"
+              className=""
+              onClick={() => {
+                onClose();
+              }}
+            >
+              X
+            </Button>
+          </div>
+        </div>
         <hr />
 
         <ListGroup as="ol">
@@ -32,14 +59,27 @@ export default function SlidingSidebar({
                 alt={row.name + "의 이미지"}
               />
               {row.name}
+              &nbsp;&nbsp;
+              {row.isDrinking ? <BiSolidDrink /> : <MdOutlineNoDrinks />}
             </ListGroup.Item>
           ))}
         </ListGroup>
 
         {/* 고정된 하단 버튼 */}
-        <div className="sidebar-footer ">
+        <div className="sidebar-footer col-9 col-sm-9 col-md-6 ">
           <InputGroup className="mb-3 d-flex">
-            <div className="col-6 mb-2">
+            <div className="col-4 mb-2">
+              <Link href={`/gatherings/${roomId}/setting`}>
+                <Button
+                  variant="outline-secondary"
+                  size="lg"
+                  className="w-100 d-flex justify-content-center align-items-center"
+                >
+                  <IoIosSettings />
+                </Button>
+              </Link>
+            </div>
+            <div className="col-4 mb-2">
               <Button
                 variant="outline-primary"
                 size="lg"
@@ -51,7 +91,7 @@ export default function SlidingSidebar({
                 <BsShare />
               </Button>
             </div>
-            <div className="col-6 mb-2">
+            <div className="col-4 mb-2">
               <Button
                 variant="outline-danger"
                 size="lg"
@@ -74,7 +114,7 @@ export default function SlidingSidebar({
           position: fixed;
           top: 0;
           right: -80%;
-          width: 80%;
+
           height: 100dvh;
           background-color: white;
           box-shadow: -2px 0 5px rgba(0, 0, 0, 0.1);
@@ -91,8 +131,9 @@ export default function SlidingSidebar({
         .sidebar-footer {
           position: absolute;
           bottom: 1px;
+          padding-right: 30px;
           background-color: white;
-          width: 70dvw;
+          width: 100%;
         }
       `}</style>
     </>
