94f3d851252ad371e38c61b4a2fe57e6eee89d04
bm150 <jbm20dev@gmail.com>
2024-10-16 19:17:21 +0900

    test : testData 삽입

--- src/app/gatherings/[roomId]/page.tsx
+++ src/app/gatherings/[roomId]/page.tsx
@@ -5,11 +5,12 @@ export const metadata: Metadata = {
   title: "모임방",
   description: "모임방 내부 페이지 입니다",
 };
+import { testTimeLineData } from "./testdata";
 
 export default function Page() {
   return (
     <>
-      <GatheringRoomPage />
+      <GatheringRoomPage timelineData={testTimeLineData} />
     </>
   );
 }
